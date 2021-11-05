83be64b9dadc97579547281680e9f61210b3bb6d
candychuang <candychuang@tencent.com>
2021-11-05 10:05:18 +0800

    feat: update arrow function

--- src/stores/project.ts
+++ src/stores/project.ts
@@ -4,20 +4,23 @@ import {EdgeConfig} from '@antv/g6';
 import {NodeConfig} from "@antv/g6-core/lib/types";
 
 const useProjectStore = defineStore('graph', {
-    state() {
+    state: () => {
         return {
             node: shallowRef<NodeConfig>(),
-            edge: shallowRef<EdgeConfig>()
+            edge: shallowRef<EdgeConfig>(),
+            edge2: undefined as EdgeConfig | undefined,
         }
     },
     actions: {
         getEdge(): EdgeConfig | undefined {
             return this.edge
         },
+        getEdge2(): EdgeConfig | undefined {
+            return this.edge2
+        },
         getNode(): NodeConfig | undefined {
             return this.node
         }
     }
-
 });
 export {useProjectStore};
